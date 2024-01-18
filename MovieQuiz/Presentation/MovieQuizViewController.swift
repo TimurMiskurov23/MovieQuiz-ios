67bab25e32f3c8efe8eaef0f68b41dbc4dc6d74a
TimurMiskurov <timurmiskurov@gmail.com>
2024-01-18 22:11:09 +0300

    переписал логику вызова методов

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -9,75 +9,72 @@ final class MovieQuizViewController: UIViewController {
     @IBOutlet private var textLabel: UILabel!
     @IBOutlet private var counterLabel: UILabel!
     
+    private let questions: [QuizQuestion] = [
+    QuizQuestion(image: "The Godfather",
+                 text: "Рейтинг этого фильма больше чем 6?",
+                 coorectAnswer: true),
+    QuizQuestion(image: "The Dark Knight",
+                 text: "Рейтинг этого фильма больше чем 6?",
+                 coorectAnswer: true),
+    QuizQuestion(image: "Kill Bill",
+                 text: "Рейтинг этого фильма больше чем 6?",
+                 coorectAnswer: true),
+    QuizQuestion(image: "The Avengers",
+                 text: "Рейтинг этого фильма больше чем 6?",
+                 coorectAnswer: true),
+    QuizQuestion(image: "Deadpool",
+                 text: "Рейтинг этого фильма больше чем 6?",
+                 coorectAnswer: true),
+    QuizQuestion(image: "The Green Knight",
+                 text: "Рейтинг этого фильма больше чем 6?",
+                 coorectAnswer: true),
+    QuizQuestion(image: "Old",
+                 text: "Рейтинг этого фильма больше чем 6?",
+                 coorectAnswer: false),
+    QuizQuestion(image: "The Ice Age Adventures of Buck Wild",
+                 text: "Рейтинг этого фильма больше чем 6?",
+                 coorectAnswer: false),
+    QuizQuestion(image: "Tesla",
+                 text: "Рейтинг этого фильма больше чем 6?",
+                 coorectAnswer: false),
+    QuizQuestion(image: "Vivarium",
+                 text: "Рейтинг этого фильма больше чем 6?",
+                 coorectAnswer: false)
+    ]
+  
+    private var currentQuestionIndex = 0
+    private var correctAnswers = 0
+//    private let currnetQuestion = questions[currentQuestionIndex]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        show(quiz: QuizStepViewModel(image: UIImage (named: "The Godfather") ?? UIImage(), question: , questionNumber: currentQuestionIndex))
+        let model = convert(model: QuizQuestion(image: questions[0].image, text: questions[0].text, coorectAnswer: questions[0].coorectAnswer))
+        show(quiz: model)
     }
     private func show(quiz step: QuizStepViewModel) {
         imageView.image = step.image
         textLabel.text = step.question
         counterLabel.text = step.questionNumber
     }
+    
+    private func convert(model: QuizQuestion) -> QuizStepViewModel {
+        let questionStep = QuizStepViewModel(image: UIImage (named: model.image) ?? UIImage(), question: model.text, questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)")
+        return questionStep
+    }
+    
     @IBAction private func noButtonClicked(_ sender: Any) {
     }
     @IBAction private func yesButtonClicked(_ sender: Any) {
     }
 
 }
-
-
 struct QuizQuestion {
     let image: String
     let text: String
     let coorectAnswer: Bool
 }
-
-private let questions: [QuizQuestion] = [
-QuizQuestion(image: "The Godfather",
-             text: "Рейтинг этого фильма больше чем 6?",
-             coorectAnswer: true),
-QuizQuestion(image: "The Dark Knight",
-             text: "Рейтинг этого фильма больше чем 6?",
-             coorectAnswer: true),
-QuizQuestion(image: "Kill Bill",
-             text: "Рейтинг этого фильма больше чем 6?",
-             coorectAnswer: true),
-QuizQuestion(image: "The Avengers",
-             text: "Рейтинг этого фильма больше чем 6?",
-             coorectAnswer: true),
-QuizQuestion(image: "Deadpool", 
-             text: "Рейтинг этого фильма больше чем 6?",
-             coorectAnswer: true),
-QuizQuestion(image: "The Green Knight",
-             text: "Рейтинг этого фильма больше чем 6?",
-             coorectAnswer: true),
-QuizQuestion(image: "Old",
-             text: "Рейтинг этого фильма больше чем 6?",
-             coorectAnswer: false),
-QuizQuestion(image: "The Ice Age Adventures of Buck Wild",
-             text: "Рейтинг этого фильма больше чем 6?",
-             coorectAnswer: false),
-QuizQuestion(image: "Tesla",
-             text: "Рейтинг этого фильма больше чем 6?",
-             coorectAnswer: false),
-QuizQuestion(image: "Vivarium",
-             text: "Рейтинг этого фильма больше чем 6?",
-             coorectAnswer: false)
-]
-
-private var currentQuestionIndex = 0
-private var correctAnswers = 0
-private let currnetQuestion = questions[currentQuestionIndex]
-
 struct QuizStepViewModel {
     let image: UIImage
     let question: String
     let questionNumber: String
 }
-
-private func convert(model: QuizQuestion) -> QuizStepViewModel {
-    let questionStep = QuizStepViewModel(image: UIImage (named: model.image) ?? UIImage(), question: model.text, questionNumber: "\(currentQuestionIndex + 1)/\(questions.count)")
-    return questionStep
-}
-
